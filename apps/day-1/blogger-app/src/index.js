77608e3b946211364d849357dd2848f9f1ff0eff
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-01-17 14:03:24 +0530

    commit html templates for components into the repo

---
+++ apps/day-1/blogger-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Header = function () {
+  return <h1>Blogger App</h1>;
+};
+
+ReactDOM.render(<Header />, document.querySelector('#root'));
